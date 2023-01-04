42e099a1f8bff8c081190f95399f0c9fe44f8134
Maaz Khan <85396969+maazkhansw5145@users.noreply.github.com>
2023-01-04 15:31:41 +0500

    URI updated

--- utils/MongodbConnect.js
+++ utils/MongodbConnect.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const MongodbConnect = async () => {
   if (!mongoose.connections[0].readyState) {
     mongoose
-      .connect("mongodb://localhost:27017/tide", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(
+        "mongodb+srv://jcxa95:Justin.Tide.codes2023@cluster0.luzmfzu.mongodb.net/?retryWrites=true&w=majority",
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
       .then(() => console.log("Database connected"))
       .catch((e) => console.log(e));
   }
